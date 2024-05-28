5f379e25366165d9fc1a50bd0dfd4c0e19e36820
Jaime <jaimecanarj@gmail.com>
2024-05-29 01:06:06 +0200

    Primer commit

---
+++ docs/js/searchIndex.js
@@ -0,0 +1,19 @@
+Search.appendIndex(
+    [
+                {
+            "fqsen": "\\obtener_numero_dias_entre_fechas\u0028\u0029",
+            "name": "obtener_numero_dias_entre_fechas",
+            "summary": "",
+            "url": "namespaces/default.html#function_obtener_numero_dias_entre_fechas"
+        },                {
+            "fqsen": "\\filtrar_numeros_mayores_a_10\u0028\u0029",
+            "name": "filtrar_numeros_mayores_a_10",
+            "summary": "",
+            "url": "namespaces/default.html#function_filtrar_numeros_mayores_a_10"
+        },                {
+            "fqsen": "\\",
+            "name": "\\",
+            "summary": "",
+            "url": "namespaces/default.html"
+        }            ]
+);
---
+++ practica-Canada.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * @author Jaime Cañada Arjona
+ * @version 1.0
+ */
+ 
+/**
+ * @param string $fecha1 Fecha inicial de nuestro intervalo
+ * @param string $fecha2 Fecha final de nuestro intervalo
+ * 
+ * @return integer Número de días que hay entre ambas fechas
+ * 
+ */
+function obtener_numero_dias_entre_fechas($fecha1, $fecha2) {
+  // Convertimos las fechas a objetos DateTime
+  $fecha_inicio = new DateTime($fecha1);
+  $fecha_fin = new DateTime($fecha2);
+ 
+  // Calculamos la diferencia entre las fechas
+  $intervalo = $fecha_inicio->diff($fecha_fin);
+ 
+  // Obtenemos el número total de días
+  $numero_dias = $intervalo->days;
+ 
+  return $numero_dias;
+}
+ 
+ /** 
+ * @param integer[] $numeros Colección de números a filtrar
+ * 
+ * @return integer[] Colección de números mayor que 10
+ */
+function filtrar_numeros_mayores_a_10($numeros) {
+    $numeros_filtrados = array();
+ 
+    foreach ($numeros as $numero) {
+        if ($numero > 10) {
+            $numeros_filtrados[] = $numero;
+        }
+    }
+ 
+    return $numeros_filtrados;
+}
+ 
+?>
